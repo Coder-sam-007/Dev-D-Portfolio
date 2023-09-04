5ad3119d3694e4ae80de2a7c0b933bce38ae5ab7
coder-sam-007 <sameerjain.coder@gmail.com>
2023-09-04 09:45:19 +0530

    menu btn and stuff added

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//header stuff
+
+const header = document.querySelector("header");
+const MenuBTN = document.querySelector(".menu-btn");
+
+MenuBTN.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
+
+const links = document.querySelectorAll("nav>ul>*");
+
+links.forEach((link) =>
+  link.addEventListener("click", () => header.classList.remove("active"))
+);
+
+//slider
+
 let swiper = new Swiper(".mySwiper", {
   cssMode: true,
   navigation: {
